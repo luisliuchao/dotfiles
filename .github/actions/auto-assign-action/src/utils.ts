f1bfe20aa5e4d025807bac2d9df053fe6141dd39
Liu Chao <luis.liu.1018@gmail.com>
2021-04-22 22:34:39 +0800

    use local action

---
+++ .github/actions/auto-assign-action/src/utils.ts
@@ -0,0 +1,139 @@
+import * as core from '@actions/core'
+import _ from 'lodash'
+import * as github from '@actions/github'
+import * as yaml from 'js-yaml'
+import { Config } from './handler'
+
+export function chooseReviewers(owner: string, config: Config): string[] {
+  const {
+    useReviewGroups,
+    reviewGroups,
+    numberOfReviewers,
+    reviewers,
+    includeOwner,
+  } = config
+  let chosenReviewers: string[] = []
+  const useGroups: boolean =
+    useReviewGroups && Object.keys(reviewGroups).length > 0
+
+  if (useGroups) {
+    chosenReviewers = chooseUsersFromGroups(
+      owner,
+      reviewGroups,
+      numberOfReviewers,
+      includeOwner
+    )
+  } else {
+    chosenReviewers = chooseUsers(reviewers, numberOfReviewers, owner)
+  }
+
+  return chosenReviewers
+}
+
+export function chooseAssignees(owner: string, config: Config): string[] {
+  const {
+    useAssigneeGroups,
+    assigneeGroups,
+    addAssignees,
+    numberOfAssignees,
+    numberOfReviewers,
+    assignees,
+    reviewers,
+    includeOwner,
+  } = config
+  let chosenAssignees: string[] = []
+
+  const useGroups: boolean =
+    useAssigneeGroups && Object.keys(assigneeGroups).length > 0
+
+  if (typeof addAssignees === 'string') {
+    if (addAssignees !== 'author') {
+      throw new Error(
+        "Error in configuration file to do with using addAssignees. Expected 'addAssignees' variable to be either boolean or 'author'"
+      )
+    }
+    chosenAssignees = [owner]
+  } else if (useGroups) {
+    chosenAssignees = chooseUsersFromGroups(
+      owner,
+      assigneeGroups,
+      numberOfAssignees || numberOfReviewers,
+      includeOwner
+    )
+  } else {
+    const candidates = assignees ? assignees : reviewers
+    chosenAssignees = chooseUsers(
+      candidates,
+      numberOfAssignees || numberOfReviewers,
+      owner
+    )
+  }
+
+  return chosenAssignees
+}
+
+export function chooseUsers(
+  candidates: string[],
+  desiredNumber: number,
+  filterUser: string = ''
+): string[] {
+  const filteredCandidates = candidates.filter((reviewer: string): boolean => {
+    return reviewer !== filterUser
+  })
+
+  // all-assign
+  if (desiredNumber === 0) {
+    return filteredCandidates
+  }
+
+  return _.sampleSize(filteredCandidates, desiredNumber)
+}
+
+export function includesSkipKeywords(
+  title: string,
+  skipKeywords: string[]
+): boolean {
+  for (const skipKeyword of skipKeywords) {
+    if (title.toLowerCase().includes(skipKeyword.toLowerCase()) === true) {
+      return true
+    }
+  }
+
+  return false
+}
+
+export function chooseUsersFromGroups(
+  owner: string,
+  groups: { [key: string]: string[] } | undefined,
+  desiredNumber: number,
+  includeOwner: boolean
+): string[] {
+  const filterUser = includeOwner ? '' : owner
+  let users: string[] = []
+  for (const group in groups) {
+    users = users.concat(chooseUsers(groups[group], desiredNumber, filterUser))
+  }
+  return users
+}
+
+export async function fetchConfigurationFile(client: github.GitHub, options) {
+  const { owner, repo, path, ref } = options
+  const result = await client.repos.getContents({
+    owner,
+    repo,
+    path,
+    ref,
+  })
+
+  const data: any = result.data
+
+  if (!data.content) {
+    throw new Error('the configuration file is not found')
+  }
+
+  const configString = Buffer.from(data.content, 'base64').toString()
+  const config = yaml.safeLoad(configString)
+  core.info(`config ${config} ${config.includeOwner ? 'true' : 'false'}`)
+
+  return config
+}
